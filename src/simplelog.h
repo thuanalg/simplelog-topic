fbefd976a09d5fee1b704468d870f1f96d6605bf
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-01-24 00:37:54 +0700

    Add "Message safety feature."

--- src/simplelog.h
+++ src/simplelog.h
@@ -356,37 +356,37 @@ __p__ = __FILE__;} while(0);
 {;SIMPLE_LOG_ST *__t__ = spl_control_obj();;\
 	if(__t__->llevel <= (__lv__) && ___fmttt___[0] && __t__->arr_topic) \
 	{\
-		;short __tpp__ = 0;int len = 0;unsigned short r = 0;;const char *__pfn__ = 0;;\
+		;short __tpp__ = 0;int len = 0;unsigned short __r__ = 0;;const char *__pfn__ = 0;;\
 		;int __outlen__ = 0;;char *__pprefmt__ = 0;; char __tnow__[SPL_RL_BUF];;__tpp__ = __tpic__%__t__->n_topic;;;\
 		; __FILLE__(__pfn__);;\
-		;__pprefmt__ = spl_fmt_now_ext(__tnow__, SPL_RL_BUF, __lv__, __pfn__, __FUNCTION__, __LINE__, &r, &__outlen__);;\
+		;__pprefmt__ = spl_fmt_now_ext(__tnow__, SPL_RL_BUF, __lv__, __pfn__, __FUNCTION__, __LINE__, &__r__, &__outlen__);;\
 		do\
 		{\
 			;;\
-			spl_mutex_lock(__t__->arr_mtx[r]);\
+			spl_mutex_lock(__t__->arr_mtx[__r__]);\
 				/*do \
 				{*/\
 					/*if(__t__->arr_topic){*/;;\
 						;;\
-						if(__t__->range > STSPLOGBUFTOPIC_RANGE(__t__,__tpp__, r)->pl) {\
-							;memcpy(STSPLOGBUFTOPIC_RANGE(__t__,__tpp__, r)->data + STSPLOGBUFTOPIC_RANGE(__t__,__tpp__, r)->pl, __pprefmt__, __outlen__);\
-							;STSPLOGBUFTOPIC_RANGE(__t__, __tpp__, r)->pl += __outlen__;;\
-							;len = snprintf(STSPLOGBUFTOPIC_RANGE(__t__,__tpp__, r)->data + STSPLOGBUFTOPIC_RANGE(__t__,__tpp__, r)->pl, \
-								__t__->krange - STSPLOGBUFTOPIC_RANGE(__t__, __tpp__, r)->pl, \
+						if(__t__->range > STSPLOGBUFTOPIC_RANGE(__t__,__tpp__, __r__)->pl) {\
+							;memcpy(STSPLOGBUFTOPIC_RANGE(__t__,__tpp__, __r__)->data + STSPLOGBUFTOPIC_RANGE(__t__,__tpp__, __r__)->pl, __pprefmt__, __outlen__);\
+							;STSPLOGBUFTOPIC_RANGE(__t__, __tpp__, __r__)->pl += __outlen__;;\
+							;len = snprintf(STSPLOGBUFTOPIC_RANGE(__t__,__tpp__, __r__)->data + STSPLOGBUFTOPIC_RANGE(__t__,__tpp__, __r__)->pl, \
+								__t__->krange - STSPLOGBUFTOPIC_RANGE(__t__, __tpp__, __r__)->pl, \
 								___fmttt___, ##__VA_ARGS__);\
 							;/*spl_console_log("--------------lllllllennnnnnnnnnnnnnnnn---r: %d, len: %d", (int)r, len);*/;\
 							if(len > 0) {\
-								;__outlen__ = SPL_MIN_AB(len, __t__->krange - STSPLOGBUFTOPIC_RANGE(__t__, __tpp__, r)->pl);;\
-								;STSPLOGBUFTOPIC_RANGE(__t__, __tpp__, r)->pl += __outlen__;\
+								;__outlen__ = SPL_MIN_AB(len, __t__->krange - STSPLOGBUFTOPIC_RANGE(__t__, __tpp__, __r__)->pl);;\
+								;STSPLOGBUFTOPIC_RANGE(__t__, __tpp__, __r__)->pl += __outlen__;\
 							}\
 						}\
 					/*}*/\
 				/*}\
 				while(0);*/\
-			spl_mutex_unlock(__t__->arr_mtx[r]);\
+			spl_mutex_unlock(__t__->arr_mtx[__r__]);\
 			if(len > 0) break;\
 			;\
-			;r++; r%=__t__->ncpu;;continue;\
+			;__r__++; __r__%=__t__->ncpu;;continue;\
 		}\
 		while(1);\
 		if(!__t__->trigger_thread)spl_rel_sem(__t__->sem_rwfile);;if(__pprefmt__ != __tnow__) { spl_free(__pprefmt__);}\
