781f5eaa1713e7f60e9ee79ab5143c0bacfcccef
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-04-17 14:30:59 +0700

    Release v1.0.4 Linux, Windows, MAC-OSX.

---
+++ src/linux/xrun.sh
@@ -0,0 +1,5 @@
+export LD_LIBRARY_PATH=./
+#./simple_cpp_example --nthread=10 --loopcount=1000000
+#./simple_cpp_example_direct --nthread=10 --loopcount=1000000
+#./simple_c_example --nthread=10 --loopcount=1000000 --topic_index=5 --cfg=z.cfg
+./simple_cpp_example --nthread=10 --loopcount=1000000 --topic_index=2 --cfg=z.cfg
