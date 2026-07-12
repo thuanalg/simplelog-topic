c2fd60e11be004b39e3d80b44c3a915c4818c315
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2026-07-12 17:08:41 +0700

    x3 - mach

--- src/mach/xrun.sh
+++ src/mach/xrun.sh
@@ -3,7 +3,9 @@ rm -rf log_simple
 #./simple_cpp_example --nthread=10 --loopcount=1000000
 #./simple_cpp_example_direct --nthread=10 --loopcount=1000000
 #./simple_c_example --nthread=10 --loopcount=100 --mainindex=1 --topic_index=0 --topic_Bindex=0 --cfg=z.cfg
-./simple_cpp_example --nthread=10 --loopcount=100 --mainindex=1 --topic_index=0 --topic_Bindex=0 --cfg=z.cfg
+#./simple_cpp_example --nthread=10 --loopcount=100 --mainindex=1 --topic_index=0 --topic_Bindex=0 --cfg=z.cfg
+./simple_cpp_example --nthread=10 --loopcount=1000000 --mainindex=1 --topic_index=0 --topic_Bindex=0 --cfg=z.cfg
+#./simple_cpp_example --nthread=10 --loopcount=1000000 --mainindex=1  --cfg=z.cfg
 
 
 #Document:
@@ -11,3 +13,5 @@ rm -rf log_simple
 #2. --loopcount=	-->> Number of times for loop. 		Default: 1 million
 #2. --nthread=		-->> Number od threads.			Default: 10
 #4. --topic_index=	-->> 0: default. [1-n] for specific index, please see in file of configuration
+#5. --topic_Bindex=	-->> 0: default. [1-m] for specific binary index, please see in file of configuration
+#6. --mainindex=	-->> >= 0: enable of wrtting main topic 
